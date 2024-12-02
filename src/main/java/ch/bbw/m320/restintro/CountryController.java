fde03c0609e4834e9f20845cc9f72ff1a7a6c52f
Armin Rüegger <armin.rueegger@ergon.ch>
2024-12-02 08:38:49 +0100

    schwulian

--- src/main/java/ch/bbw/m320/restintro/CountryController.java
+++ src/main/java/ch/bbw/m320/restintro/CountryController.java
@@ -10,42 +10,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/country")
-public class PonyController {
+public class CountryController {
 
 	private final List<Country> list = new ArrayList<>();
 
-	public PonyController() {
+	public CountryController() {
 		// we fill the "database" with initial data.
 		list.add(new Country(1, "USA", 4800000,  30000, 30008834, 20000 ));
 	}
 
 	@GetMapping
-	public List<PonyDto> getPonies() {
+	public List<Country> getCountries() {
 		return list;
 	}
 
 	@PostMapping
-	public ResponseEntity<String> newPony(@RequestBody PonyDto newPony) {
-		if (newPony.color.equals("ugly")) {
-			throw new NoUglyPoniesException();
-		}
-		list.add(newPony);
+	public ResponseEntity<String> newCountry(@RequestBody Country newCountry) {
+		list.add(newCountry);
 		return ResponseEntity.status(HttpStatus.CREATED).header("X-Fuu", "Bar").body("totally worked");
 	}
-
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	static class NoUglyPoniesException extends IllegalArgumentException {
-
-		public NoUglyPoniesException() {
-			super("ponies are not ugly");
-		}
-	}
-
-	public record PonyDto(String name, String color) {}
 }
