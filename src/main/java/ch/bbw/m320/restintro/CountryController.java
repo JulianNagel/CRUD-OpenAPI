0a2aeb549d5a666ddc98b72ea1dcabc5f3350e91
Armin Rüegger <armin.rueegger@ergon.ch>
2024-12-02 08:56:12 +0100

    julian schrieb tests

--- src/main/java/ch/bbw/m320/restintro/CountryController.java
+++ src/main/java/ch/bbw/m320/restintro/CountryController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static java.util.Arrays.stream;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/api/country")
@@ -27,22 +29,28 @@ public class CountryController {
     @GetMapping("/id/{id}")
     public Country getCountryWithID(@PathVariable int id) {
         return list.stream()
-				.filter(country -> country.id == id)
-				.findFirst()
-				.orElseThrow(() -> new IllegalArgumentException("Country not found: " + id));
+                .filter(country -> country.id == id)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Country not found: " + id));
     }
 
     @GetMapping("/name/{name}")
     public Country getCountryWithName(@PathVariable String name) {
-		return list.stream()
-				.filter(country -> name.equals(country.name))
-				.findFirst()
-				.orElseThrow(() -> new IllegalArgumentException("Country not found: " + name));
+        return list.stream()
+                .filter(country -> name.equals(country.name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Country not found: " + name));
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<String> updateCountry(@RequestBody Country newCountry, @PathVariable int id) {
+        list.get(id).name = newCountry.name;
+        return ResponseEntity.status(HttpStatus.CREATED).header("Update", "Bar").body("totally worked");
     }
 
     @PostMapping
     public ResponseEntity<String> newCountry(@RequestBody Country newCountry) {
         list.add(newCountry);
-        return ResponseEntity.status(HttpStatus.CREATED).header("X-Fuu", "Bar").body("totally worked");
+        return ResponseEntity.status(HttpStatus.CREATED).header("Create", "Bar").body("totally worked");
     }
 }
